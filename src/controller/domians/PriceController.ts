c47a8e8b41edd661aa3085bb01a099146bee5b22
_MrVapor <info@florianrichter.eu>
2021-08-15 02:43:43 +0200

    remove async from functions

--- src/controller/domians/PriceController.ts
+++ src/controller/domians/PriceController.ts
@@ -2,19 +2,19 @@ import RubUtils from "../../utils/rubUtils";
 import {PriceCsvResponse, PriceResponse, PromotionPriceResponse} from "../../types/domains/price";
 
 export default class PriceController {
-    public async getAllPrices(): Promise<PriceResponse> {
+    public getAllPrices(): Promise<PriceResponse> {
         return RubUtils.call('/domain/price', { method: 'GET' })
     }
 
-    public async getAllPricesAsCSVFile(): Promise<PriceCsvResponse> {
+    public getAllPricesAsCSVFile(): Promise<PriceCsvResponse> {
         return RubUtils.call('/domain/price/csv', { method: 'GET' })
     }
 
-    public async getAllPromotionPrices(): Promise<PromotionPriceResponse> {
+    public getAllPromotionPrices(): Promise<PromotionPriceResponse> {
         return RubUtils.call('/domain/price/promotion', { method: 'GET' })
     }
 
-    public async getAllPromotionPricesAsCSVFile(): Promise<PriceCsvResponse> {
+    public getAllPromotionPricesAsCSVFile(): Promise<PriceCsvResponse> {
         return RubUtils.call('/domain/price/promotion/csv', { method: 'GET' })
     }
 }
